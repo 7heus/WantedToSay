2c599e29e0da2d157e5c97dda52e0c0515c3933c
Theus <matheusgoias@hotmail.com>
2024-08-27 17:57:17 +0100

    Create index.js

---
+++ WantedToSay-react-app/src/lib/api/index.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+const urlString = "http://localhost:3000/api";
+
+export const getMessages = async () => {
+  try {
+    const response = await axios.get(`${urlString}/messages`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getQueryMessage = async (query) => {
+  try {
+    const response = await axios.get(
+      `${urlString}/messages?q=${query.replace(" ", "_")}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const postMessage = async (content, encryptionKey, recipient) => {
+  const formattedData = {
+    receiver: recipient,
+    content: content,
+    secretKey: encryptionKey,
+  };
+  try {
+    const response = await axios.post(`${urlString}/messages`, formattedData);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getUserKey = async (userId) => {
+  try {
+    const response = await axios.get(`${urlString}/users/${userId}`);
+    return response.data.uniqueKey;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const decryptMessages = async (data, secretKey) => {
+  console.log(data);
+  try {
+    const response = await axios.post(`${urlString}/messages/decrypt`, {
+      secretKey: secretKey,
+      data: [data],
+    });
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const decryptMessage = async (data, secretKey) => {
+  try {
+    const response = await axios.post(
+      `${urlString}/messages/decrypt/${data._id}`,
+      {
+        secretKey: secretKey,
+        data: [...data],
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
