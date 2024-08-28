23aa55dce38a938d9cade8fa78593002871d5d57
CarlosCwebdev <CarlosCarvalho1995@hotmail.com>
2024-08-28 15:12:23 +0100

    66

--- WantedToSay-react-app/src/components/Navbar.jsx
+++ WantedToSay-react-app/src/components/Navbar.jsx
@@ -1,16 +1,25 @@
 import "./Navbar.css";
-
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
 function Navbar() {
-
-
-
+  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
   return (
     <nav className="Navbar">
-        <h1>Navbar</h1>
+      <Link to="/">
+        <button>Home</button>
+      </Link>
+      {!isLoggedIn && (
+        <>
+          <Link to="/signup">
+            <button className="SignUp">Sign Up</button>
+          </Link>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+        </>
+      )}
     </nav>
   );
 }
-
-
-
 export default Navbar;
