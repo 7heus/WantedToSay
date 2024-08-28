7558503a15081398f30a89cf3ea0941885067077
Theus <matheusgoias@hotmail.com>
2024-08-28 12:22:27 +0100

    color conditional

--- WantedToSay-react-app/src/components/NoteCard.jsx
+++ WantedToSay-react-app/src/components/NoteCard.jsx
@@ -7,22 +7,35 @@ export default function NoteCard({ data }) {
   const [content, setContent] = useState("");
   useEffect(() => {
     if (data) {
-      decryptMessages(data, "c001k3y").then((dat) => {
-        if (dat && dat.data && dat.data.length > 0) {
-          setContent(dat.data[0].content);
-        }
-      }).catch((error) => {
-        console.error("Error decrypting message:", error);
-      });
+      decryptMessages(data, "c001k3y")
+        .then((dat) => {
+          if (dat && dat.data && dat.data.length > 0) {
+            setContent(dat.data[0].content);
+          }
+        })
+        .catch((error) => {
+          console.error("Error decrypting message:", error);
+        });
     }
   }, [data]);
-  
+
   useEffect(() => console.log(content), [content]);
   return (
     <Link to={`/messages/${data._id}`}>
       <div className="Card">
-        <div className="content" style={{}}>
-          <p className="content">{content && content}</p>
+        <p className="recipient">{data.receiver}</p>
+        <div
+          className="content"
+          style={{
+            backgroundColor: data ? data.color : "white",
+          }}
+        >
+          <p
+            className="content"
+            style={{ color: data.color == "white" ? "black" : "white" }}
+          >
+            {content && content}
+          </p>
         </div>
       </div>
     </Link>
