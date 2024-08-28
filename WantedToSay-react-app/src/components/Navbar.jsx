50788d84a1e85d5fa7857efb70759bfd4f7e2e1c
Theus <matheusgoias@hotmail.com>
2024-08-28 16:20:51 +0100

    Update Navbar.jsx

--- WantedToSay-react-app/src/components/Navbar.jsx
+++ WantedToSay-react-app/src/components/Navbar.jsx
@@ -2,47 +2,29 @@ import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
-<<<<<<< HEAD
-function Navbar() {
-  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
-=======
 
 function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
->>>>>>> FrontEndDeveloping
   return (
     <nav className="Navbar">
       <Link to="/">
         <button>Home</button>
       </Link>
-<<<<<<< HEAD
-      {!isLoggedIn && (
-        <>
-          <Link to="/signup">
-            <button className="SignUp">Sign Up</button>
-=======
 
       {!isLoggedIn && (
         <>
           <Link to="/signup">
             <button>Sign Up</button>
->>>>>>> FrontEndDeveloping
           </Link>
           <Link to="/login">
             <button>Login</button>
           </Link>
         </>
       )}
-<<<<<<< HEAD
-    </nav>
-  );
-}
-export default Navbar;
-=======
       {isLoggedIn && (
         <>
-          <span>Welcome, {user && user.username}</span>
+          <span>Welcome, {user && user.name}</span>
           <button onClick={logOutUser}>Logout</button>
         </>
       )}
@@ -51,4 +33,3 @@ export default Navbar;
 }
 
 export default Navbar;
->>>>>>> FrontEndDeveloping
