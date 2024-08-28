e0a7ec71fcb68896617b05452d0b8643c9ed3dbc
Theus <matheusgoias@hotmail.com>
2024-08-28 17:40:40 +0100

    Create UserProfile.jsx

---
+++ WantedToSay-react-app/src/pages/UserProfile.jsx
@@ -0,0 +1,70 @@
+import { useContext } from "react";
+import { useState, useEffect } from "react";
+import { AuthContext } from "../context/auth.context";
+import { updateUser } from "../lib/crud";
+import "./UserProfile.css";
+
+export default function UserProfile() {
+  const { user } = useContext(AuthContext);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [uniqueKey, setUniqueKey] = useState("");
+
+  const handleName = (e) => setName(e.target.value);
+  const handleEmail = (e) => setEmail(e.target.value);
+  const handleKey = (e) => setUniqueKey(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formattedData = {
+      name,
+      email,
+      uniqueKey: uniqueKey ? uniqueKey : "c001k3y",
+    };
+    updateUser(user._id, formattedData);
+    return false;
+  };
+
+  useEffect(() => {
+    if (user) {
+      setUniqueKey(user.uniqueKey);
+      setEmail(user.email);
+      setName(user.name);
+    }
+  }, [user]);
+
+  return (
+    <div className="page">
+      <div className="card">
+        <p>Your settings,</p>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Name:{" "}
+            <input type="text" name="name" value={name} onChange={handleName} />
+          </label>
+          <label>
+            Email:{" "}
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={handleEmail}
+            />
+          </label>
+          <label>
+            Encryption Key:{" "}
+            <input
+              type="password"
+              name="encryption"
+              value={uniqueKey}
+              onChange={handleKey}
+            />
+          </label>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+  // something
+}
