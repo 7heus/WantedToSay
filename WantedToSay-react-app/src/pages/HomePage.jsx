f92aed2118b30abecbbba2be764492561d4a6c0d
Theus <matheusgoias@hotmail.com>
2024-08-28 09:15:50 +0100

    Update HomePage.jsx

--- WantedToSay-react-app/src/pages/HomePage.jsx
+++ WantedToSay-react-app/src/pages/HomePage.jsx
@@ -1,13 +1,22 @@
+import NoteCard from "../components/NoteCard";
+import { getMessages } from "../lib/api";
+import { useState, useEffect } from "react";
 
-    
 function HomePage() {
-    return (
-      <div>
-        <h1>WantedToSay</h1>
-        <h2>Say it with no worries!!!</h2>
-      </div>
-    );
-  }
-  
-  export default HomePage;
-  
+  const [cards, setCards] = useState(null);
+
+  useEffect(() => {
+    getMessages().then((dat) => setCards(dat));
+  }, []);
+
+  return (
+    <div>
+      <h1>WantedToSay</h1>
+      <h2>Say it with no worries!!!</h2>
+      {cards &&
+        cards.map((card, index) => <NoteCard data={card} key={index} />)}
+    </div>
+  );
+}
+
+export default HomePage;
