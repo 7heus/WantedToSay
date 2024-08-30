d4cf8c50ae54a584f6d6b2b8ba942475715444ca
CarlosCwebdev <CarlosCarvalho1995@hotmail.com>
2024-08-30 16:28:50 +0100

    searchBar1

--- src/pages/MessagePage.jsx
+++ src/pages/MessagePage.jsx
@@ -4,10 +4,12 @@ import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
 import messageService from "../services/auth.message.service";
 import NoteCard from "../components/NoteCard";
+import SearchBar from "../components/searchBar";
 import "./MessagePage.css";
 
 function MessagePage() {
   const [messages, setMessages] = useState([]);
+  const [filteredMessages, setFilteredMessages] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
   const { isLoggedIn } = useContext(AuthContext);
   const navigate = useNavigate();
@@ -20,6 +22,7 @@ function MessagePage() {
         .getMessages()
         .then((response) => {
           setMessages(response.data);
+          setFilteredMessages(response.data);
         })
         .catch((error) => {
           console.error("Error fetching messages:", error);
@@ -28,21 +31,39 @@ function MessagePage() {
     }
   }, [isLoggedIn, navigate]);
 
+  const handleSearch = (query) => {
+    if (query) {
+      setFilteredMessages(
+        messages.filter((msg) =>
+          msg.receiver?.toLowerCase().includes(query.toLowerCase())
+        )
+      );
+    } else {
+      setFilteredMessages(messages);
+    }
+  };
+  
+  
+
   return (
     <div className="MessagePage">
-      <h1>Messages</h1>
+      <h1 className="Messages">Messages</h1>
 
       {errorMessage && <p className="error-message">{errorMessage}</p>}
 
+      <SearchBar onSearch={handleSearch} />
+
       <div className="cards-container">
-        {messages.length > 0 ? (
-          messages.map((msg, index) => <NoteCard data={msg} key={index} />)
+        {filteredMessages.length > 0 ? (
+          filteredMessages.map((msg, index) => <NoteCard data={msg} key={index} />)
         ) : (
           <p>No messages available.</p>
         )}
       </div>
 
-      <button onClick={() => navigate("/new-message")} className="sendMessageBtn">Send New Message</button>
+      <button onClick={() => navigate("/new-message")} className="sendMessageBtn">
+        Send New Message
+      </button>
     </div>
   );
 }
