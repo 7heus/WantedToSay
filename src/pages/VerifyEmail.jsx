a3738bc9e504a0a14d46086d068c5be648296f79
Theus <matheusgoias@hotmail.com>
2024-09-03 10:35:52 +0100

    setState, redirecting instead of closing

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { verifyEmail } from "../lib/crud";
 
-export default function VerifyUser() {
+export default function VerifyUser({ setVerifying }) {
   const [loading, setLoading] = useState(true);
   const [serverResponse, setServerResponse] = useState("");
   const { userId } = useParams();
   const nav = useNavigate();
 
   useEffect(() => {
+    setVerifying(true);
     verifyEmail(userId)
       .then((data) => {
         setLoading(false);
@@ -20,7 +21,7 @@ export default function VerifyUser() {
   }, []);
 
   useEffect(() => {
-    setTimeout(() => window.close(), 5000);
+    setTimeout(() => nav("/messages"), 3000);
   }, [loading]);
 
   return (
